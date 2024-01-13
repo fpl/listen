3cc15fd33fa14bfbd7c471c7d5a82d863ef4c6e7
Francesco Paolo Lovergine <frankie@debian.org>
2024-01-13 15:25:22 +0100

    Added copyright template.

--- listen.c
+++ listen.c
@@ -1,5 +1,20 @@
 /*
  * listen.c - server that accept connection on an available TCP port.
+ *
+ * Copyright (C) 1999-2024  Francesco P. Lovergine <frankie@linux.com> 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
@@ -79,6 +94,3 @@ main(int argc,char **argv)
   }
   exit(0);
 }
-
-
-
